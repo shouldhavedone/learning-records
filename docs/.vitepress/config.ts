83a2e164e1c28059a0325445ce99c7f4d13856bd
shouldhavedone <wt15182161727@163.com>
2024-05-09 10:09:32 +0800

    页面导入样式时，使用link和@import有什么区别

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -18,6 +18,15 @@ export default defineConfig({
           },
         ],
       },
+      {
+        text: 'CSS',
+        items: [
+          {
+            text: '页面导入样式时，使用link和@import有什么区别',
+            link: '/css/页面导入样式时，使用link和@import有什么区别',
+          },
+        ],
+      },
     ],
 
     sidebar: [
@@ -30,6 +39,15 @@ export default defineConfig({
           {
             text: '写H5和小程序有什么相同及不同的地方',
             link: '/html/写H5和小程序有什么相同及不同的地方',
+          },
+        ],
+      },
+      {
+        text: 'CSS',
+        items: [
+          {
+            text: '页面导入样式时，使用link和@import有什么区别',
+            link: '/css/页面导入样式时，使用link和@import有什么区别',
           },
         ],
       },
--- docs/css/页面导入样式时，使用link和@import有什么区别.md
+++ docs/css/页面导入样式时，使用link和@import有什么区别.md
@@ -1,2 +1,9 @@
 # 页面导入样式时，使用link和@import有什么区别
 
+* link是XHTML标签，除了加载CSS外，还可以定义RSS等其他事务；@import属于CSS范畴，只能加载CSS
+
+* link引用CSS时，在页面载入时同时加载；@import需要页面网页完全载入以后加载，所以会出现一开始没有css样式，闪烁一下出现样式后的页面(网速慢的情况下)
+
+* link是XHTML标签，无兼容问题；@import是在CSS2.1提出的，低版本的浏览器不支持。
+
+* link支持使用Javascript控制DOM去改变样式；而@import不支持。
